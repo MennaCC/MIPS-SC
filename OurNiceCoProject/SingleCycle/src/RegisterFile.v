f98d0033f33c3d95b0ae44fbd27c5dac0efef631
Menna CC <mkudo94@gmail.com>
2015-12-18 16:27:49 +0200

    added the no. of its for the inputs in the reg file

--- OurNiceCoProject/SingleCycle/src/RegisterFile.v
+++ OurNiceCoProject/SingleCycle/src/RegisterFile.v
@@ -1,7 +1,9 @@
 module RegisterFile (rdData1,rdData2,
 	                 rdReg1,rdReg2,wtReg,wtData,regWrite);
 	
-	input  rdReg1, rdReg2, wtReg,wtData,regWrite;
+	input [0:4]  rdReg1, rdReg2, wtReg;
+	input [0:31] wtData;
+	input regWrite;
 	output rdData1,rdData2;	
 	
 	reg [0:31] registers [0:31];

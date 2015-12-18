0674eb9efa7b0d767a46b09b32e153904d772419
Shimaa Lotfy <meshmesha559@gmail.com>
2015-12-18 11:32:42 -0800

    zbat el hpal eli kont 3mlah

--- OurNiceCoProject/SingleCycle/src/SignExtend.v
+++ OurNiceCoProject/SingleCycle/src/SignExtend.v
@@ -1,10 +1,9 @@
-module SignExtend(clk ,In , Out );
+module SignExtend(In , Out );
 	input [15:0] In ;
-	input clk ;
 	output [31:0] Out ;
 	reg [31:0] Out ;
 	
-	always @(posedge clk)begin 	
+	always @(In)begin 	
 		Out[15:0] <= In;
 		Out[31:16] <= {16{In[15]}};
 	end 
